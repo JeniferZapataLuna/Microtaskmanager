60abdad25d7100f228345f69871f3a056f0da57a
AndresSPRA <159939295+AndresSPRA@users.noreply.github.com>
2024-05-06 21:45:07 -0500

    Footer terminado
    footer de la pagina web terminado

---
+++ Vite/src/Pages/footer.jsx
@@ -0,0 +1,27 @@
+import './footer.css'
+
+function Foot() {
+  return (
+    <>
+    <footer>
+        <hr className="lineaBL"></hr>
+        <a href="">
+            <img className="logo" src="public/img/github-mark-white.png" alt="logo de hithub"/>
+        </a>
+        <section className="foot">
+            <h1 className="foot">
+                Microtaskmanager Github
+            </h1>
+            <p className="foot">
+                Copyright  2024
+            </p>
+        </section>
+
+
+    </footer>
+
+    </>
+  )
+}
+
+export default Foot
