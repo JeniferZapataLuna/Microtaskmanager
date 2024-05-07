98879a5b3bdf307a15b8bc9f2f7fd34e7dff15a9
JeniferZapataLuna <jenifer.zapata801@pascualbravo.edu.co>
2024-05-06 20:15:06 -0500

    commit

--- Vite/src/App.jsx
+++ Vite/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
 
   return (
-    <>
+    <div className='D1'>
+    <img 
+    src='public/img/menu.png' alt="Rayas" className='menu'/>
     <h1>Hola mundo</h1>
-    </>
+    </div>
   )
 }
 
