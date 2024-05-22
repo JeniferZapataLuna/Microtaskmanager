3a4af0f4f8f71dd3786415bfe60a1ae07615dfde
JeniferZapataLuna <jenifer.zapata801@pascualbravo.edu.co>
2024-05-22 00:22:56 -0500

    Update Inicio_Sesion.jsx

--- Vite/src/Pages/Inicio_Sesion.jsx
+++ Vite/src/Pages/Inicio_Sesion.jsx
@@ -14,12 +14,12 @@ const Inicio_Sesion = ({ closePopup }) => {
         <div className='modal_container' id='modal_container'>
             <div className='modal'>
                 <label type="text" className='til'>Iniciar Sesión</label>
-               <div className='Form'>
+               <form className='Form'>
                     <span className="close-button" onClick={closePopup}>&times;</span>
-                    <input type="text" className='form Usuario'  placeholder="Usuario"/>
-                    <input type="text" className='form Contraseña'  placeholder="Contraseña"/>
+                    <input type="text" name='Usuario' className='form Usuario' id='FormU_Usuario' placeholder="Usuario"/>
+                    <input type="text" name='Contraseña' className='form Contraseña'  id='FormU_Contraseña' placeholder="Contraseña"/>
                     <button type='button' className='form Ingresar hover-effect'>Ingresar</button>
-                </div>
+                </form>
                 <div className="main-container">
                 <p>¿Necesitas una cuenta? <a href="#" onClick={toggleModal} className="open-modal-link">Regristrarse</a>
                   {isModalOpen && <Modal closeModal={toggleModal} />}</p>
