e9d08d59364944972e6b8244f667eefd2aed4c13
JeniferZapataLuna <jenifer.zapata801@pascualbravo.edu.co>
2024-05-28 23:17:03 -0500

    commit
    Implementación de MUI en el footer

--- Vite/src/Pages/footer.jsx
+++ Vite/src/Pages/footer.jsx
@@ -1,28 +1,64 @@
-import './css/footer.css'
+import React from 'react';
+import {
+  Box,
+  Typography,
+  Divider,
+  Link,
+  Container
+} from '@mui/material';
 
 const Foot = () => {
   return (
-    <>
-    <footer>
-        <hr className="lineaBL"></hr>
-        <a href="https://github.com/JeniferZapataLuna/Microtaskmanager">
-            <img className="logo" src="/Img/github.png" alt="logo de hithub"/>
-            
-        </a>
-        <section className="foot">
-            <h1 className="foot">
-                Microtaskmanager Github
-            </h1>
-            <p className="foot">
-                Copyright © 2024
-            </p>
-        </section>
-
-
-    </footer>
-
-    </>
-  )
+    <Box
+      component="footer"
+      sx={{
+        backgroundColor: '#FFFAFA',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        textAlign: 'center',
+        padding: '1rem 0',
+      }}
+    >
+      <Divider sx={{ width: '90%', borderWidth: '2px', borderColor: 'rgb(0, 0, 0)', borderRadius: '4px', marginBottom: '1rem' }} />
+      <Link href="https://github.com/JeniferZapataLuna/Microtaskmanager" target="_blank" rel="noopener">
+        <Box
+          component="img"
+          sx={{
+            width: '4em',
+            height: '4em',
+            marginBottom: '1rem',
+          }}
+          alt="logo de Github"
+          src="/Img/github.png"
+        />
+      </Link>
+      <Container sx={{ textAlign: 'center' }}>
+        <Typography
+          variant="h5"
+          sx={{
+            fontFamily: 'Montserrat, sans-serif',
+            fontWeight: 800,
+            color: 'rgb(0, 0, 0)',
+            margin: '0.2em',
+          }}
+        >
+          Microtaskmanager Github
+        </Typography>
+        <Typography
+          variant="body2"
+          sx={{
+            fontFamily: 'Montserrat, sans-serif',
+            fontWeight: 800,
+            color: 'rgb(0, 0, 0)',
+            margin: '0.2em',
+          }}
+        >
+          Copyright © 2024
+        </Typography>
+      </Container>
+    </Box>
+  );
 }
 
-export default Foot
+export default Foot;
